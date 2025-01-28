590d9a1d6f45dd8ea3028131da8b1f69c4c021a9
ayoub lahlaibi <lhlaibiayoub@gmail.com>
2025-01-28 21:46:37 +0100

    Delete TODO directory

--- TODO/php/include/nav.php
+++
@@ -1,9 +0,0 @@
-<nav class="navbar bg-body-tertiary">
-  <div class="container-fluid">
-    <h1 class="text-center w-100" >
-      <span class="navbar-text text-center w-100">
-          To-do List 
-      </span>
-    </h1>
-  </div>
-</nav>
